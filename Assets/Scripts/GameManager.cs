246cf73940ab782e98609fb5c5a5a0c530199197
Miguel Orozco <m68476521.1@gmail.com>
2025-04-19 11:53:36 -0700

    Completing the game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,42 @@
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    
+    public GameObject block;
+    public float maxX;
+    public Transform spawnPoint;
+    public float spawnRate;
+
+    bool gameStarted = false;
+
+    public GameObject tapText;
+    public TextMeshProUGUI scoreText;
+
+    int score = 0;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !gameStarted )
+        {
+            StartSpawing();
+            gameStarted = true;
+            tapText.SetActive(false);
+        }
+    }
+
+    private void StartSpawing() 
+    {
+        InvokeRepeating("SpawnBlock", 0.5f, spawnRate);
+    }
+
+    void SpawnBlock() {
+        Vector3 spawnPos = spawnPoint.position;
+
+        spawnPos.x = Random.Range( -maxX, maxX);
+
+        Instantiate(block, spawnPos, Quaternion.identity);
+        score++;
+        scoreText.text = score.ToString();
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player: MonoBehaviour
 {
@@ -6,7 +7,7 @@ public class Player: MonoBehaviour
     Rigidbody2D rb;
 
     void Start()
-    { Debug.Log("Direction: START");
+    {
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -15,16 +16,6 @@ public class Player: MonoBehaviour
         if (Input.GetMouseButton(0)) 
         {
             Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            // // 1. Get the mouse position in world coordinates
-            // Vector3 mousePos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -cam.transform.position.z));
-
-            // // // 2. Calculate the direction to move towards
-            // Vector3 direction = (mousePos - rb.position).normalized;
-
-            // // // 3. Apply a force to move the Rigidbody
-            // rb.AddForce(direction * moveSpeed, ForceMode2D.Impulse);
-            // Debug.Log("Direction: " + direction);
             if (touchPos.x < 0)
             {
                 rb.AddForce( Vector2.left * moveSpeed );
@@ -33,50 +24,18 @@ public class Player: MonoBehaviour
             {
                 rb.AddForce( Vector2.right * moveSpeed );
             }
-            // var mousePos = Input.mousePosition;
-
-            //         mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-
-            //         var mouseDir = mousePos - gameObject.transform.position;
-            //         mouseDir = new Vector3(mouseDir.x, 0f, mouseDir.z);
-            //         mouseDir = mouseDir.normalized;
-
-            //         rb.AddForce((mouseDir) * 1500f * Time.smoothDeltaTime);
-
-                    Debug.Log("Direction: Update");
         }
         else
         {
-            Debug.Log("Direction: ELSE");
-            rb.velocity = Vector2.zero;
+            rb.linearVelocity = Vector2.zero;
         }
     }
 
-    void FixedUpdate() 
+    private void OnCollisionEnter2D(Collision2D collition)
     {
-
-         Debug.Log("Direction: IF");
-        // float moveInput = Input.GetAxis("Horizontal");
-        // Vector2 movement = new Vector2(moveInput * moveSpeed, rb.velocity.y);
-
-        // rb.velocity = movement;
-
-        //     Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        
-        //   if (touchPos.x < 0)
-        //     {
-        //         rb.AddForce( Vector2.left * moveSpeed );
-        //     }
-        //     else
-        //     {
-        //         rb.AddForce( Vector2.right * moveSpeed );
-        //     }
-
+        if (collition.gameObject.tag == "Block")
+        {
+            SceneManager.LoadScene("Game"); // or 0
+        }
     }
-//     void FixedUpdate() {
-//        float horizontalInput = Input.GetAxis("Horizontal");
-//        Vector2 currentVelocity = rb.linearVelocity; // Get current velocity
-//        currentVelocity.x = horizontalInput * moveSpeed; // Update x-component
-//        rb.velocity = currentVelocity; // Set the new velocity
-//    }
 }
